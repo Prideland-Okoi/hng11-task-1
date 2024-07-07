378d183d5565b4c816888c7063a84ce6beb49297
Prideland-Okoi <prideland.okoi@gmail.com>
2024-07-07 15:02:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,33 +7,45 @@ load_dotenv()
 
 app = Flask(__name__)
 
-WEATHER_ENDPOINT = "http://api.weatherapi.com/v1/current.json"
-weather_api_key = os.getenv('API_KEY')
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
-    visitor_name = request.args.get('visitor_name', 'Visitor')
+    visitor_name = request.args.get('visitor_name', 'Guest')
+    # client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
     client_ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0].strip()
+    weather_api_key = os.getenv('WEATHER_API_KEY')
 
     try:
-        location_response = requests.get(f'https://ip-api.com/json/{client_ip}')
+        # Get location data using IP
+        location_response = requests.get(f'https://ipapi.co/{client_ip}/json/')
         location_data = location_response.json()
-        city = location_data.get('city', 'Unknown location')
+        location = location_data.get('city', 'None')
 
-        if city != 'Unknown location':
-            weather_response = requests.get(WEATHER_ENDPOINT, params={'key': weather_api_key, 'q': city})
-            weather_data = weather_response.json()
-            temperature = weather_data['current']['temp_c']
-        else:
-            temperature = 'N/A'
+        if location is None:
+            return jsonify({"error": "Location not found"}), 404
+
+        # Log location data for debugging
+        print(f"Location data: {location_data}")
+
+        # Get weather data using location
+        weather_response = requests.get(
+            f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={weather_api_key}&units=metric')
+        weather_data = weather_response.json()
+
+        # Log weather data for debugging
+        print(f"Weather data: {weather_data}")
+
+        temperature = weather_data['main']['temp']
 
         return jsonify({
-            'client_ip': client_ip,
-            'location': city,
-            'greeting': f'Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {city}'
+            "client_ip": client_ip,
+            "location": location,
+            "greeting": f"Hello, {visitor_name}! The temperature is {temperature} degrees Celsius in {location}"
         })
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        # Log the error for debugging
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
